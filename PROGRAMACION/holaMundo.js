77b9e5d44dbc940e5cc698d3d7bfcd7d0f6733ca
sebastianvillarrealsc <sebastianvillarealsc@gmail.com>
2023-03-26 08:50:28 -0300

    repaso domingo

---
+++ PROGRAMACION/holaMundo.js
@@ -0,0 +1,3 @@
+console.log("hola mundo soy la linea uno del archivo typescrip");
+console.log("hol mundo soy la linea dos del archivo typescrip");
+var nombrevariable = 5;
---
+++ PROGRAMACION/holaMundo.ts
@@ -0,0 +1,5 @@
+console.log("hola mundo soy la linea uno del archivo typescrip");
+console.log("hol mundo soy la linea dos del archivo typescrip");
+
+
+var nombrevariable = 5;
